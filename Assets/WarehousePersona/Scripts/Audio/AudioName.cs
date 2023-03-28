242785a0e8584e7387b143888960e79c20a6fd46
Alok Kumar Dubey <aloksonu.dubey@gmail.com>
2023-03-28 12:49:58 +0530

    First Base Commit

---
+++ Assets/WarehousePersona/Scripts/Audio/AudioName.cs
@@ -0,0 +1,12 @@
+﻿namespace Audio.Warehouse
+{
+    public enum AudioName
+    {
+
+        NotSet=-1,
+        GameBG = 0,
+        ButtonClick = 1,
+        Correct = 2,
+        Wrong = 3,
+    }
+}
