73678d01a968a225c43610e6ec03d2b854803360
Alok Kumar Dubey <aloksonu.dubey@gmail.com>
2023-03-30 07:14:18 +0530

    WIP camera view change

--- Assets/WarehousePersona/Scripts/WarehousePersonaManager.cs
+++ Assets/WarehousePersona/Scripts/WarehousePersonaManager.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class WarehousePersonaManager : MonoBehaviour
 {
     [SerializeField] Transport transport;
+    [SerializeField] GameObject transportEnvoirnment;
+    [SerializeField] AssignGate assignGate;
+    [SerializeField] GameObject assignGateEnvoirnment;
     private bool isTransportCompleted;
+    private bool isCallAssignGate;
+
     void Start()
     {
         isTransportCompleted = false;
+        isCallAssignGate = false;
         NarratorPanel.Instance.BringInNarrator(NarratorPanel.Instance.NInbound);
     }
 
     void FixedUpdate()
     {
-        if (transport.transform.position.x < 38 && isTransportCompleted == false)
+        if (transport.transform.position.x < 38 && isTransportCompleted == false && isCallAssignGate == false)
         {
             if (transport.isMove == true)
             {
@@ -28,12 +34,25 @@ public class WarehousePersonaManager : MonoBehaviour
             }
         }
 
-        else if (transport.transform.position.x >= 38)
+        else if (transport.transform.position.x >= 38 && isCallAssignGate == false)
         {
             Fader.Instance.BringIn();
             isTransportCompleted = true;
             transport.rbd2.velocity = Vector2.left * 0;
             Debug.Log("Greater Value");
+
+            if (isCallAssignGate == false)
+            this.Invoke("callAssignGate", 2f);
         }
+    }
+
+    private void callAssignGate()
+    {
+       // transport.gameObject.SetActive(false);
+        transportEnvoirnment.SetActive(false);
+       // assignGate.gameObject.SetActive(true);
+        assignGateEnvoirnment.SetActive(true);
+        Fader.Instance.BringOut();
+        isCallAssignGate = true;
     }
 }
