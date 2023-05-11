a0b44ef9b24c4d4f6510d72f9bfa341a48dba9ca
Alok Kumar Dubey <aloksonu.dubey@gmail.com>
2023-05-11 17:40:10 +0530

    drag 2d done

--- Assets/WarehousePersona/Sandbox/DragAndDrop2d.cs
+++ Assets/WarehousePersona/Sandbox/DragAndDrop2d.cs
@@ -6,10 +6,13 @@ public class DragAndDrop2d : MonoBehaviour
 {
     Vector3 mousePosition;
     Vector3 initialPosition;
+    [SerializeField] private Transform otherTransform;
+    private bool isDropDone;
 
     void Start()
     {
         initialPosition = this.transform.position;
+        isDropDone = false;
     }
 
     private Vector3 GetMousePosition()
@@ -19,17 +22,47 @@ public class DragAndDrop2d : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if(isDropDone==false)
         mousePosition = Input.mousePosition - GetMousePosition();
     }
 
     private void OnMouseDrag()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+        if (isDropDone == false)
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
     }
 
     private void OnMouseUp()
     {
-        transform.position = initialPosition;
+        float distance = Vector3.Distance(transform.position, otherTransform.position);
+        if (distance > 1.5f)
+        {
+            transform.position = initialPosition;
+        }
+        else
+        {
+            isDropDone = true;
+            transform.position = otherTransform.position;
+        }
+        Debug.Log("mouse up");
     }
 
+    //private void Update()
+    //{
+    //    float distance = Vector3.Distance(transform.position, otherTransform.position);
+    //    Debug.Log(distance);
+    //    if (distance <= 1.5f && isMouseUp == true)
+    //    {
+    //        transform.position = otherTransform.position;
+    //    }
+    //}
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    Debug.Log("colide");
+    //    if (other.gameObject.GetComponent<ProcessName>().strProcessName == "put" && isMouseUp == true)
+    //    {
+    //        Debug.Log("AssignGate Done");
+    //        transform.position = otherTransform.position;
+    //    }
+    //}
 }
